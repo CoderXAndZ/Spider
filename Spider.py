75253f27c34e85453df194defccd05289bb85798
1935722630@qq.com <admin@admindeMacBook-Pro.local>
2019-01-14 20:54:05 +0800

    提交

--- Spider.py
+++ Spider.py
@@ -21,18 +21,26 @@ def open_firefox(page, keywords):
     global current_page
     url = 'https://www.baidu.com/baidu?tn=monline_3_dg&ie=utf-8&wd=%E8%9E%8D%E6%89%98%E9%87%91%E8%9E%8D'
     try:
-        page_num = '?page=%s' % page
+        # page_num = '?page=%s' % page
         current_page = int(page)
-        wd.get(url+page_num)
+        # wd.get(url+page_num)
+        wd.get(url)
         wait = WebDriverWait(wd, timeout=13)
         # # wait.until(expected_conditions.title_is("平台"))
         # # wait.until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, '.link-login'))
 
-        print("页面内容：", wd.page_source)
+        # print("页面内容：", wd.page_source)
+        # 获取页面内容
         html_content = wd.page_source
         soup = BeautifulSoup(html_content, 'html.parser')
-        # 获取公司的列表
-        companies = soup.find('div', id='content_left').select('ul > li')
+        # 获取内容列表
+        companies = soup.find('div', id='content_left')
+        print('获取内容列表：', companies)
+        array_result = []
+        for i in range(1, 10):
+            div_one = companies.select('class="result c-container"')
+            array_result.append(div_one)
+        print("获取的数组：", array_result)
 
     except NoSuchElementException as e:
         print("异常信息：", e.msg)
