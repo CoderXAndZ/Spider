c2328fd3eb67b82e4b06178936278e06a84ea1e8
1935722630@qq.com <admin@admindeMacBook-Pro.local>
2019-01-15 18:50:19 +0800

    标题、链接、简介获取

---
+++ GUITool.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3
+# -*- coding: UTF-8 -*-
+# 页面画
+
+import tkinter as tk
+
+if __name__ == '__main__':
+    root = tk.Tk()            # 创建父容器GUI
+    root.title("搜索结果")     # 父容器标题
+    root.geometry("460x380")  # 设置父容器窗口初始大小，如果没有这个设置，窗口会随着组件大小的变化而变化
--- Spider.py
+++ Spider.py
@@ -14,7 +14,7 @@ wd = webdriver.Firefox()
 current_window = wd.current_window_handle
 
 
-# 打开浏览器
+# 打开浏览器, 搜索关键字
 def open_firefox(page, keywords):
     # url = 'https://www.baidu.com/s?wd=%E8%9E%8D%E6%89%98%E9%87%91%E8%9E%8D&pn=10&oq=%E8%9E%8D%E6%89%98%E9%87%91%E8%9E%8D&tn=monline_3_dg&ie=utf-8&usm=1&rsv_pq=8ef13f780000d1b5&rsv_t=f1a8letyGDxgzrFTe%2BTXGUVodY1icqi2d3bmB8S9WJqSkjL80kJTOnwlAdr6nkpPNWPb&rsv_page=1'
 
@@ -38,12 +38,38 @@ def open_firefox(page, keywords):
         print('获取内容列表：', companies)
         array_result = []
         for i in range(1, 10):
-            div_one = companies.select('class="result c-container"')
+            div_one = companies.find('div', id=str(i))
             array_result.append(div_one)
-        print("获取的数组：", array_result)
+            # h3标签中的a链接的内容
+            title = div_one.find('h3', class_='t').a.contents  # 题目  a.contents 获取链接的内容
+            title = str(title).replace('<em>', '').replace('</em>', '')  # 删除<em> 和 </em>
+            href = div_one.find('h3', class_='t').a['href']  # 链接
+            abstract = div_one.find('div', class_='c-abstract')  # 内容 .contents
+            if abstract is not None:
+                # 去除其他字符，只剩文字
+                abstract = str(abstract.contents).replace('<div class="c-abstract">', '').\
+                    replace('</div>', '').replace('<em>', '').replace('</em>', '').\
+                    replace('<span class="newTimeFactor_before_abs m">', '').replace('</span>', '').replace('\'', '')
+            else:
+                abstract = "无"
+            print("title是：", title)
+            print("href链接是：", href)
+            print("abstract简介是：", abstract)
+            create_txt_write_in(title, href, abstract)
+        # print("获取的数组：", array_result)
 
     except NoSuchElementException as e:
         print("异常信息：", e.msg)
+
+
+# 创建txt文件，逐行写入
+def create_txt_write_in(title, href, abstract):
+    txt_name = "searchResult.txt"
+    f = open(txt_name, "a+")
+    f.write('标题：' + title + '\n')
+    f.write('链接：' + href + '\n')
+    f.write('简介：' + abstract + '\n\n')
+    f.close()
 
 
 # 判断元素是否存在
